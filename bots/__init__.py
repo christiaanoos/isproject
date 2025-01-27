fc94284b5c20f4b81cc4c7a7cabd911eb228b061
christiaanoos <christiaanoosterbeek@outlook.com>
2025-01-27 14:59:07 +0100

    Add files via upload

---
+++ bots/__init__.py
@@ -0,0 +1,17 @@
+"""Create a bot in a separate .py and import them here, so that one can simply import
+it by from schnapsen.bots import MyBot.
+"""
+from .rand import RandBot
+from .alphabeta import AlphaBetaBot
+from .rdeep import RdeepBot
+from .ml_bot import MLDataBot, MLPlayingBot, train_ML_model
+from .gui.guibot import SchnapsenServer
+from .minimax import MiniMaxBot
+from .bully_bot import BullyBot
+from .Minimaxhighpriority import MinimaxhighpriorityBot
+from .Rdeephighpriority import RdeephighpriorityBot
+from .TwoPhaseBot import TwoPhaseBot
+from .Randhighpriority import RandhighpriorityBot
+
+
+__all__ = ["RandBot", "RandhighpriorityBot", "TwoPhaseBot", "AlphaBetaBot", "RdeepBot", "MLDataBot", "MLPlayingBot", "train_ML_model", "SchnapsenServer", "MiniMaxBot", "MinimaxhighpriorityBot", "BullyBot", "RdeephighpriorityBot"]
